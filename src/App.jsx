828dc8b6c338c48eb916f59c02759a7de95ef196
karishma352 <karishmasinghjayant@users.noreply.github.com>
2025-08-14 11:43:40 +0530

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import Second from "./second"
+function App(){
+  return(
+  <div>
+    <Second/>
+    <h1>FIRST REACT</h1>
+    <h2>LINKING ANOTHER JSX FILE</h2>
+  </div>
+  )
+}
+
+export default App
---
+++ src/Second.jsx
@@ -0,0 +1,14 @@
+
+
+
+function Second(){
+  return(
+    <div>
+      <h1>second file</h1>
+      <h2>check up</h2>
+      <h3>hii</h3>
+    </div>
+  )
+}
+
+export default Second;
